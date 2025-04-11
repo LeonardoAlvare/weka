b7f77ff4752a3e5d6634310a4752bdbbfb44d78e
Leonardo Alvarez <leonardoalvarezr12@gmail.com>
2025-04-10 21:50:54 -0500

    cambio de prediccion

--- src/main/java/com/importweka/weka/services/dto/DatasetDTO.java
+++ src/main/java/com/importweka/weka/services/dto/DatasetDTO.java
@@ -6,13 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DatasetDTO {
-    private double userId_x;
-    private double projectId;
-    private String requiredExperience;
-    private String proposalsReceived;
-    private String popularidadCreator;
-    private Boolean isFreelance;
-    private String category;
-    private double salary;
-    private double timeLimit;
+    private String status_x; // aceptado o rechazado
+    private String requiredExperience; // junior, semi-senior, senior
+    private String proposalsReceived; // alto, bajo, medio
+    private String popularidadCreator; // alto, bajo, medio
+    private String category; // backend, frontend, mobile, nube, web
+    private String academicTraining; // backend, frontend, mobile, nube, web
 }
