b7b11a94e028ea9bca3c9eb9e0b193873325a266
Leonardo Alvarez <leonardoalvarezr12@gmail.com>
2025-05-05 14:58:36 -0500

    actualizacion de prediccion

--- src/main/java/com/importweka/weka/services/DatasetService.java
+++ src/main/java/com/importweka/weka/services/DatasetService.java
@@ -12,9 +12,9 @@ import com.importweka.weka.services.dto.DatasetResponseDTO;
 import jakarta.annotation.PostConstruct;
 import weka.classifiers.Classifier;
 import weka.core.DenseInstance;
-import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Attribute;
 
 @Service
 public class DatasetService {
@@ -24,8 +24,10 @@ public class DatasetService {
 
     @PostConstruct
     public void init() throws Exception {
-        InputStream modelStream = getClass().getClassLoader().getResourceAsStream("dataset_final.model");
-        InputStream arffStream = getClass().getClassLoader().getResourceAsStream("dataset_final.arff");
+        // InputStream modelStream =
+        // getClass().getClassLoader().getResourceAsStream("dataset_model.model");
+        InputStream modelStream = getClass().getClassLoader().getResourceAsStream("dataset_model.model");
+        InputStream arffStream = getClass().getClassLoader().getResourceAsStream("freelancer_dataset_diverso.csv.arff");
 
         if (modelStream == null || arffStream == null) {
             throw new Exception("No se pudo cargar el modelo o la estructura ARFF");
@@ -33,19 +35,23 @@ public class DatasetService {
 
         model = (Classifier) weka.core.SerializationHelper.read(modelStream);
         dataStructure = new Instances(new java.io.BufferedReader(new java.io.InputStreamReader(arffStream)));
-        // dataStructure.setClassIndex(dataStructure.numAttributes() - 1);
-        dataStructure.setClassIndex(0);
+        dataStructure.setClassIndex(dataStructure.numAttributes() - 1);
     }
 
     public DatasetResponseDTO prediccion(DatasetDTO input) throws Exception {
         Instance instance = new DenseInstance(dataStructure.numAttributes());
         instance.setDataset(dataStructure);
 
-        instance.setValue(dataStructure.attribute("requiredExperience"), input.getRequiredExperience());
-        instance.setValue(dataStructure.attribute("proposalsReceived"), input.getProposalsReceived());
-        instance.setValue(dataStructure.attribute("popularidadCreator"), input.getPopularidadCreator());
+        instance.setValue(dataStructure.attribute("Edad"), input.getEdad());
+        instance.setValue(dataStructure.attribute("YearsExperience"), input.getYearsExperience());
+        instance.setValue(dataStructure.attribute("EducationLevel"), input.getEducationLevel());
+        instance.setValue(dataStructure.attribute("Languages"), input.getLanguages());
+        instance.setValue(dataStructure.attribute("WorkAvailability"), input.getWorkAvailability());
         instance.setValue(dataStructure.attribute("category"), input.getCategory());
-        instance.setValue(dataStructure.attribute("academicTraining"), input.getAcademicTraining());
+        instance.setValue(dataStructure.attribute("DeveloperLevel"), input.getDeveloperLevel());
+        instance.setValue(dataStructure.attribute("PaymentProposal"), input.getPaymentProposal());
+        instance.setValue(dataStructure.attribute("EstimatedTime"), input.getEstimatedTime());
+        instance.setValue(dataStructure.attribute("categoryProject"), input.getCategoryProject());
 
         double[] distribution = model.distributionForInstance(instance);
         Attribute classAttribute = dataStructure.classAttribute();
@@ -56,7 +62,7 @@ public class DatasetService {
 
         for (int i = 0; i < distribution.length; i++) {
             String className = classAttribute.value(i);
-            double porcentaje = Math.round(distribution[i] * 10000.0) / 100.0; // 2 decimales
+            double porcentaje = Math.round(distribution[i] * 10000.0) / 100.0;
             probabilidades.put(className, porcentaje);
 
             if (distribution[i] > maxProb) {
--- src/main/java/com/importweka/weka/services/dto/DatasetDTO.java
+++ src/main/java/com/importweka/weka/services/dto/DatasetDTO.java
@@ -6,10 +6,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DatasetDTO {
-    private String status_x; // aceptado o rechazado
-    private String requiredExperience; // junior, semi-senior, senior
-    private String proposalsReceived; // alto, bajo, medio
-    private String popularidadCreator; // alto, bajo, medio
-    private String category; // backend, frontend, mobile, nube, web
-    private String academicTraining; // backend, frontend, mobile, nube, web
+    // private String nombre;
+    private int edad;
+    private int yearsExperience;
+    private String educationLevel; // 0: Bachiller, 1: Semi-técnico, 2: Tecnólogo, 3: Profesional
+    private String languages; // 1: Español, 2: Inglés, 3: Ambos
+    private String workAvailability; // 0: Tiempo completo, 1: Medio tiempo
+    private String category; // 0: Web, 1: Mobile, 2: Backend, 3: Fullstack
+    private String developerLevel; // 0: Junior, 1: Semi-Senior, 2: Senior
+    private int paymentProposal;
+    private int estimatedTime;
+    private String categoryProject; // 0: Web, 1: Mobile, 2: Backend, 3: Fullstack
 }
